eefcece81487aa6c2db0499a2452c3d71c972c40
Cactussman <mustafa.mghazanfar@aiesec.net>
2017-04-20 19:04:39 +0930

    Mustafa Changed

--- JavaGit/src/JavaGit.java
+++ JavaGit/src/JavaGit.java
@@ -18,7 +18,7 @@ public class JavaGit {
         for (int i = 0; i < 10; i++) {
             System.out.println(">.<");
         }
-        System.out.println("Borrrrriiiiiinnnng");
+        System.out.println("test2");
     }
 
 }
