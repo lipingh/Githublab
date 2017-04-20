35e7f7951c07e7ab550f391616ccb3454fd35905
huang <huang@172.29.81.178>
2017-04-20 19:13:40 +0930

    v2

--- JavaGit/src/JavaGit.java
+++ JavaGit/src/JavaGit.java
@@ -16,6 +16,11 @@ public class JavaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hello");
+        // You' r ehere
+        for (int i = 0; i < 10; i++) {
+            System.out.println(">.<");
+        }
+        System.out.println("Borrrrriiiiiinnnng");
     }
-    
+
 }
